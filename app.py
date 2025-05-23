9a8fb2b25fcfbafcfad85a3a86d16af514230d1d
xcode <harshitasree346@gmail.com>
2025-05-23 15:50:01 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+print("Welcome to the Python Quiz!")
+score = 0
+
+answer = input("What is the output of 2 + 2? ")
+if answer == "4":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+print("Your score is:", score)
